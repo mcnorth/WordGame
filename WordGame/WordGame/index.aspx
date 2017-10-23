9dafbade161a81ce98a72ef39b21223a48093ffc
DESKTOP-LKCNPUO\kelli <kelliemcnaughton@gmail.com>
2017-10-23 13:13:55 +1000

    Added json.net api to read json file

---
+++ WordGame/WordGame/index.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="WordGame.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Word Game</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WordGame/WordGame/index.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace WordGame
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string path = System.Web.HttpContext.Current.Server.MapPath("~/files/words.json");
+            StreamReader rdr = new StreamReader(path);
+            string json = rdr.ReadToEnd();
+           
+            var wordList = JsonConvert.DeserializeObject<List<Word>>(json);
+
+        }
+    }
+}
